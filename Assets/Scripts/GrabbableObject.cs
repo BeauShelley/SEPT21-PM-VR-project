c4427c7ec83d2e7d55a864432c83879c74819d13
BeauShelley <91709282+BeauShelley@users.noreply.github.com>
2021-10-07 20:06:56 -0700

    Created XR rig, hand controls

---
+++ Assets/Scripts/GrabbableObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabbableObject : MonoBehaviour
+{
+
+    public Color hoverColor;
+
+    private Color defaultColor;
+    private Material material;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+        defaultColor = material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnHoverStart()
+    {
+        material.color = hoverColor;
+    }
+
+    public void OnHoverEnd()
+    {
+        material.color = defaultColor;
+    }
+
+    public void OnGrabStart(Grabber hand)
+    {
+        transform.SetParent(hand.transform);
+        GetComponent<Rigidbody>().useGravity = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void OnGrabEnd()
+    {
+        transform.SetParent(null);
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+}
